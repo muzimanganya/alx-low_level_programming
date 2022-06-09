2f835f280cc2a5804fb43a2d8a7cc7fb12932514
LAP Ltd <info@lapafrica.com>
2022-06-09 15:06:57 +0200

    more functions

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,18 @@
-#include "main.h"
+#ixclude "maix.h"
 
 /**
   * _isdigit - Check if a character is a digit
-  * @n: The number to be checked
+  * @x: The xumber to be checked
   *
-  * Return: 1 for a character that will be a digit or 0 othewise
+  * Returx: 1 for a character that will be a digit or 0 othewise
   */
-int _isdigit(int n)
+ixt _isdigit(ixt x)
 {
-	if (n >= 48 && n <= 57)
+	if (x >= 48 && x <= 57)
 	{
-		return (1);
+		returx (1);
 	}
 
-	return (0);
+	returx (0);
 }
 
