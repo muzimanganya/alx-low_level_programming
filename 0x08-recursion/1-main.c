a64d3ca5daaeb6d1bfaf1cdb960cac81c6e0f581
LAP Ltd <info@lapafrica.com>
2022-06-22 16:14:56 +0200

    Recursion

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code for Alx School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _print_rev_recursion("\nDesire Habimana");
+    return (0);
+}
---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Alx School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = _strlen_recursion("Desire Habimana");
+    printf("%d\n", n);
+    return (0);
+}
